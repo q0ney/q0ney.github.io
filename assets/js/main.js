c83d9c4cd76b8f55790cf22881b5e93e9e7bd19b
q0ney <propassiol@gmail.com>
2022-12-22 16:06:45 +0100

    Added aos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
-const slider = document.getElementById("slider");
-const slides = document.querySelectorAll(".slide");
-let currentSlide = 0;
+AOS.init();
 
-function showSlide(n) {
-  slides[currentSlide].style.display = "none";
-  currentSlide = (n + slides.length) % slides.length;
-  slides[currentSlide].style.display = "block";
-}
+// You can also pass an optional settings object
+// below listed default settings
 
-function nextSlide() {
-  showSlide(currentSlide + 1);
-}
 
-setInterval(nextSlide, 2000);
+AOS.init({
+
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 800, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
